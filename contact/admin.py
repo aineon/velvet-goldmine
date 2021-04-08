bcedca261e12077a08f998a5bcef5f882dfccdd8
aineon <enia.oneill@gmail.com>
2021-04-08 17:21:09 +0000

    Add contact views, forms and urls

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message, NewsletterSubscription
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_subscribed',)
+
+
+admin.site.register(NewsletterSubscription, NewsletterAdmin)
+admin.site.register(Message)
--- contact/views.py
+++ contact/views.py
@@ -1,2 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import NewsletterSubscription
+from .forms import ContactForm, SubscriptionForm
+
+
+def contact(request):
+    template = "contact/contact.html"
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             "Message Sent! We'll be in touch shortly!")
+
+        else:
+            messages.error(request, 'Message failed to send.'
+                           ' Please ensure the form is valid.')
+
+    form = ContactForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def newsletter_signup(request):
+
+    news_sub_form = SubscriptionForm(request.POST or None)
+    news_sub_redirect = request.POST.get('news_sub_redirect')
+
+    if news_sub_form.is_valid():
+        instance = news_sub_form.save(commit=False)
+        if (NewsletterSubscription.objects.filter(
+                email=instance.email).exists()):
+            messages.error(request, 'Sorry! This email address \
+                           is already registered for our Newsletter.')
+        else:
+            instance.save()
+            messages.success(request, "Congratulations! "
+                             " You've been added to our mailing list!")
+    return redirect(news_sub_redirect)
 
