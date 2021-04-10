cb0352447880b481ba2728317590a5311ef675cb
aineon <enia.oneill@gmail.com>
2021-04-10 18:07:09 +0000

    Add blog view, urls, form and template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+
+def all_posts(request):
+    """A view to show all blog posts"""
+    posts = BlogPost.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
