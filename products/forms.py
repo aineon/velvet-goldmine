9911ab4f701214243c1166feb13229e349c1e190
aineon <enia.oneill@gmail.com>
2021-04-16 16:09:40 +0000

    Tweak styling for responsiveness. Adjust form fields

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Product, Category, Subcategory
 from .widgets import CustomClearableFileInput
 
 
@@ -7,7 +7,9 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('category', 'subcategory', 'sku', 'name', 'slug',
+                  'description', 'has_sizes', 'price', 'image_url',
+                  'image',)
 
     image = forms.ImageField(label='Image', required=False,
                              widget=CustomClearableFileInput)
@@ -15,8 +17,12 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+        subcategories = Subcategory.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        sub_friendly_names = [(s.id,
+                              s.get_friendly_name()) for s in subcategories]
 
         self.fields['category'].choices = friendly_names
+        self.fields['subcategory'].choices = sub_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'rounded-0 input-shadow'
