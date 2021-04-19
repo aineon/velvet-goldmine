b088ed4f72833250ce34c2c722d10e3551788e33
aineon <enia.oneill@gmail.com>
2021-04-19 15:00:50 +0000

    Debug set to false

--- velvet_goldmine/settings.py
+++ velvet_goldmine/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPEMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPEMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['the-velvet-goldmine.herokuapp.com', 'localhost']
 
