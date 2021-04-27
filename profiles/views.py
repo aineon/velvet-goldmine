2bde5fe6e92227c8dcdc0df1f46a1607d3503f44
aineon <enia.oneill@gmail.com>
2021-04-27 20:10:11 +0000

    Add modal to deactivate account tab. Adjust toast message for Info update

--- profiles/views.py
+++ profiles/views.py
@@ -21,7 +21,7 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
+            messages.success(request, 'Delivery info updated successfully')
         else:
             messages.error(request, 'Update failed.'
                            'Please ensure the form is valid.')
