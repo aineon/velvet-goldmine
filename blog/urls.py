05f05fae2597f08529c6ea113750530b51ca0eae
aineon <enia.oneill@gmail.com>
2021-04-28 18:00:39 +0000

    Add confirm delete modal to blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.all_blogposts, name='all_blogposts'),
     path('<int:post_id>/', views.blogpost_detail, name='blogpost_detail'),
     path('add/', views.add_blogpost, name='add_blogpost'),
-     path('edit/<int:post_id>', views.edit_blogpost, name='edit_blogpost'),
-    path('delete/<int:post_id>', views.delete_blogpost, name='delete_blogpost'),
+    path('edit/<int:post_id>', views.edit_blogpost, name='edit_blogpost'),
+    path('delete/<int:post_id>', views.delete_blogpost,
+         name='delete_blogpost'),
 ]
